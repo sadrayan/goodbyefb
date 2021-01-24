fa9f0681018a5f970db73c97c946f5cc993ce567
Sadra Abri <sabrishamkar@connected.io>
2021-01-24 01:00:15 -0500

    amplify storage, auth, lambda trigger

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import Amplify, { Storage } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+const App = () => {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const downloadUrl = async () => {
+    const downloadUrl = await Storage.get('picture.jpg', { expires: 300 });
+    window.location.href = downloadUrl
+  }
+
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    try {
+      setLoading(true);
+      // Upload the file to s3 with private access level. 
+      await Storage.put('picture.jpg', file, {
+        level: 'private',
+        contentType: 'application/zip'
+      });
+      // Retrieve the uploaded file to display
+      const url = await Storage.get('picture.jpg', { level: 'private' })
+      setImageUrl(url);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Upload an Image </h1>
+      {loading ? <h3>Uploading...</h3> : <input
+        type="file" accept='application/zip'
+        onChange={(evt) => handleChange(evt)}
+      />}
+      <div>
+        {imageUrl ? <img style={{ width: "30rem" }} src={imageUrl} /> : <span />}
+      </div>
+      <div>
+        <h2>Download URL?</h2>
+        <button onClick={() => downloadUrl()}>Click Here!</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+// withAuthenticator wraps your App with a Login component
+export default withAuthenticator(App);
+
